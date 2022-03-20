cf55922e3c296b2bbffe8dbc4949c3b3cfd3fa91
Kathleen Juell <kathleenjuell@gmail.com>
2022-03-20 12:59:56 -0400

    Add some page templates

--- client/pages/blog/index.tsx
+++ client/pages/blog/index.tsx
@@ -2,25 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 
-const Home: NextPage = () => {
+const BlogHome: NextPage = () => {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <Head>
-                <title>Demo Next.js App</title>
+                <title>Blog Homepage</title>
                 <link rel="icon" href="/favicon.png" />
             </Head>
 
             <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-                <h1 className="text-6xl font-bold">Next.js Demo</h1>
+                <h1 className="text-6xl font-bold">Welcome to the Blog</h1>
 
                 <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
                     <div className="mt-6 w-96 rounded-xl border p-6 text-left">
-                        <Link href="/blog">
+                        <Link href="/blog/posts">
                             <a className="text-2xl font-bold text-blue-600 hover:text-black focus:text-black">
-                                Explore the Content
+                                Blog Posts
                             </a>
                         </Link>
-                        <p className="mt-4 text-xl">Find in-depth resources on technical subjects.</p>
+                        <p className="mt-4 text-xl">Find in-depth articles on technical subjects.</p>
                     </div>
                 </div>
             </main>
@@ -28,4 +28,4 @@ const Home: NextPage = () => {
     )
 }
 
-export default Home
+export default BlogHome
---
+++ client/pages/learnings/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <Head>
+                <title>Course Index</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                <h1 className="text-6xl font-bold">All Courses</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home
