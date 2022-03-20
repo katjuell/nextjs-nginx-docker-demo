eef7988b0fa9a0f72f4321697f14727f2f96be86
Kathleen Juell <kathleenjuell@gmail.com>
2022-03-20 16:12:52 -0400

    Finish login/out on client

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,47 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useEffect, useState, useCallback } from 'react'
 
 const Home: NextPage = () => {
+    const [isMounted, setIsMounted] = useState<boolean>(false)
+    const [hasUser, setHasUser] = useState<boolean>(false)
+    const callRoute = async (route: string): Promise<void> => {
+        try {
+            await fetch(`http://localhost:3001/${route}`, {
+                method: 'get',
+                credentials: 'include',
+            })
+            setUser()
+        } catch (err) {
+            console.error(`Error: ${err}`)
+        }
+    }
+    const getCookies = (): string => {
+        const getAllCookies: { [index: string]: string } = document.cookie
+            .split(';')
+            .reduce((key, string) => Object.assign(key, { [string.split('=')[0].trim()]: string.split('=')[1] }), {})
+        const user = getAllCookies.loginCookie
+
+        return user
+    }
+    const setUser = useCallback(() => {
+        const user = getCookies()
+
+        user === undefined ? setHasUser(true) : setHasUser(false)
+    }, [])
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+    useEffect(() => {
+        if (isMounted) {
+            setUser()
+        }
+    }, [isMounted, setUser])
+    useEffect(() => {
+        setUser()
+    }, [hasUser, setUser])
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <Head>
@@ -11,6 +50,17 @@ const Home: NextPage = () => {
             </Head>
 
             <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                <div className="mb-20 flex w-full items-center justify-end px-48 text-center">
+                    {hasUser ? (
+                        <button onClick={() => callRoute('login')} className="font-bold text-blue-600">
+                            Login
+                        </button>
+                    ) : (
+                        <button onClick={() => callRoute('logout')} className="font-bold text-blue-600">
+                            Logout
+                        </button>
+                    )}
+                </div>
                 <h1 className="text-6xl font-bold">Next.js Demo</h1>
 
                 <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
